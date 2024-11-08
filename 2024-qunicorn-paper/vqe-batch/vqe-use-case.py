0d9754d7fe6fda4361032cc95b8e1160ca2a617f
Julian Obst <julian.obst@iaas.uni-stuttgart.de>
2024-11-08 14:33:15 +0100

    Implement optimization loop

--- 2024-qunicorn-paper/vqe-batch/vqe-use-case.py
+++ 2024-qunicorn-paper/vqe-batch/vqe-use-case.py
@@ -1,10 +1,9 @@
 import numpy as np
+from time import sleep
 from scipy.optimize import minimize
 from qiskit.circuit.library import EfficientSU2
-from qiskit.quantum_info import Statevector
 from qiskit.quantum_info import SparsePauliOp
 from qiskit.opflow.primitive_ops import PauliSumOp
-from qiskit.algorithms import VQE
 from qiskit import QuantumCircuit
 
 import requests
@@ -22,117 +21,109 @@ pauli_lists = pauli_operator.paulis.group_commuting(qubit_wise=True)
 #print(pauli_lists)
 
 
-# create VQE example
-problem_size = int(np.log2(len(operator_matrix)))
-ans = EfficientSU2(num_qubits=problem_size, reps=2)
-
-
-# decompose the algorithm into the circuits that are executed
-init = np.random.rand(len(ans.parameters))*2*np.pi
-circ1 = ans.assign_parameters(init)
-circ2 = ans.assign_parameters(init)
-circ3 = ans.assign_parameters(init)
-
-# Add Y measurement
-circ1.sdg(0)
-circ1.h(0)
-# Add X measurement
-circ1.h(1)
-
-# ZZ needs no additional
-
-# Add IX/ZX measurement
-circ3.h(1)
-
-circ1.measure_all()
-circ2.measure_all()
-circ3.measure_all()
-
-# Base URL for the API endpoint
-origin = 'http://localhost:8080'
-
-# Initialize deployment ID (will be set later if successful)
-deployment_id = None
-
-# put circuits into a deployment for batch execution
-payload = {
-  "programs": [
-    {
-      "quantumCircuit": circ1.qasm(),
-      "assemblerLanguage": "QASM2"
-    },
-    {
-      "quantumCircuit": circ2.qasm(),
-      "assemblerLanguage": "QASM2"
-    },
-    {
-      "quantumCircuit": circ3.qasm(),
-      "assemblerLanguage": "QASM2"
-    }
-  ],
-  "name": "VQEDeployment"
-}
-
-print("Creating Deployment")
-# Send POST request to create a deployment
-deployment_response = requests.post(f'{origin}/deployments/', json=payload)
-print(deployment_response.url)
-print(deployment_response.status_code)
-
-input("Press ENTER to continue...\n")
-
-if deployment_response.status_code == 201:
-  # Extract deployment ID from the response
-  content = deployment_response.json()
-  deployment_id = content['id']
-
-  # Payload dictionary for creating a job
-  payload = { 
-    "name": "TestJob",
-    "providerName": "IBM",
-    "deviceName": "aer_simulator",
-    "shots": 1000,
-    "token": "",
-    "type": "RUNNER",
-    "deploymentId": deployment_id 
-  }
+def create_circuits():
+  problem_size = int(np.log2(len(operator_matrix)))
+  ans = EfficientSU2(num_qubits=problem_size, reps=2)
+  circuits = [ans.copy() for i in range(3)]
   
-  print("Deploying Job")
-  # Send POST request to create a job
-  job_response = requests.post(f'{origin}/jobs/', json=payload)
-  print(job_response.url)
-  print(job_response.status_code)
+  # Add Y measurement
+  circuits[0].sdg(0)
+  circuits[0].h(0)
+  # Add X measurement
+  circuits[0].h(1)
+  # ZZ needs no additional
 
-input("Press ENTER to continue...\n")
+  # Add IX/ZX measurement
+  circuits[2].h(1)
 
-if job_response.status_code == 201:
-  # Extract job ID from the response
-  content = job_response.json()
-  job_id = content['id']
-  print("Requesting Results")
-  # Send GET request to retrieve job results
-  result = requests.get(f'{origin}/jobs/{job_id}/results/')
-  print(result.url)
-  print(result.status_code)
-  input("Press ENTER to continue...\n")
+  # Add measurement
+  for c in circuits:
+    c.measure_all()
 
-### Postprocessing ###
+  return circuits
+  
 
-# aggregate result
-if result.status_code == 200:
-  # Process the results
-  all_counts = []
-  for res in result.json():
-    if res['resultType'] == 'COUNTS':
-      counts = res['data']
-      all_counts.append(counts)
-      print(counts)
+def execute_on_qunicorn(circuits, origin='http://localhost:8080'):
+  # put circuits into a deployment for batch execution
+  programs = [{"quantumCircuit": c.qasm(), "assemblerLanguage": "QASM2"} for c in circuits]
+
+  payload = {
+    "programs": programs,
+    "name": "VQEDeployment"
+  }
+
+  # Send POST request to create a deployment
+  #print("Creating Deployment")
+  deployment_response = requests.post(f'{origin}/deployments/', json=payload)
+  #print(deployment_response.url)
+  #print(deployment_response.status_code)
+
+#  input("Press ENTER to continue...\n")
+
+  if deployment_response.status_code == 201:
+    # Extract deployment ID from the response
+    content = deployment_response.json()
+    deployment_id = content['id']
+
+    # Payload dictionary for creating a job
+    payload = { 
+      "name": "TestJob",
+      "providerName": "IBM",
+      "deviceName": "aer_simulator",
+      "shots": 1000,
+      "token": "",
+      "type": "RUNNER",
+      "deploymentId": deployment_id 
+    }
+    
+    #print("Deploying Job")
+    # Send POST request to create a job
+    job_response = requests.post(f'{origin}/jobs/', json=payload)
+    #print(job_response.url)
+    #print(job_response.status_code)
+
+
+  # get info of job
+  job_id = job_response.json()['id'] 
+  job_state = job_response.json()['state'] 
+
+  while job_state in ["READY", "RUNNING"]:
+    sleep(0.05)
+    job_state = requests.get(f'{origin}/jobs/{job_id}').json()['state']
+
+
+
+  if job_response.status_code == 201:
+    # Extract job ID from the response
+    content = job_response.json()
+    job_id = content['id']
+    #print("Requesting Results")
+    # Send GET request to retrieve job results
+    result = requests.get(f'{origin}/jobs/{job_id}/results/')
+    #print(result.url)
+    #print(result.status_code)
+
+  # aggregate result
+  if result.status_code == 200:
+    # Process the results
+    all_counts = []
+    for res in result.json():
+      if res['resultType'] == 'COUNTS':
+        counts = res['data']
+        all_counts.append(counts)
+
+  return all_counts
+
+
+def objective(x):
+  final_meas_circuits = [c.assign_parameters(x) for c in param_circuits]
+  # execute circuits
+  result_counts = execute_on_qunicorn(final_meas_circuits)
 
   exp = 0.0
-  print(len(pauli_lists))
   for i in range(len(pauli_lists)):
       paulis = pauli_lists[i]
-      counts = all_counts[i]
+      counts = result_counts[i]
 
       # compute contribution of each pauli operator (p_exp)
       for pauli in paulis:
@@ -148,4 +139,14 @@ if result.status_code == 200:
           exp += pauli_operator.coeffs[idx] * p_exp
 
   print("VQE expectation", exp.real)
+  return exp.real
 
+
+# create VQE example
+param_circuits = create_circuits()
+
+np.random.seed(42)
+initial_param_values = np.random.rand(len(param_circuits[0].parameters))*2*np.pi
+
+res = minimize(objective, initial_param_values, method="COBYLA", options={"maxiter": 200})   
+print(res)
